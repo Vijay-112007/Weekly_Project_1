3531c96cd69822d69339a10131fe541b38e8d51b
Kattamudi Vijaya sai <kattamudivijaysai@gmail.com>
2025-10-22 19:14:43 +0530

    Updated Code and Final Code

--- weekly.cpp
+++ weekly.cpp
@@ -22,7 +22,7 @@ int main(){
             case 1:
             float celsius;
             celsius = 5.0/9.0*(body_temperature-32);
-            cout << "The Body Temparature in Celsius is " << celsius << endl;
+            cout << "The Body Temparature in Celsius is " << celsius << " C" << endl;
         //Now we will ask the user again if he wants to perform the operation or not
             cout << "Enter Your Choice (1-3):\n1.Fahrenheit to Celsius\n2.Celsius to Fahrenheit\n3.Exit" << " ";
             cin >> choice;
@@ -32,7 +32,7 @@ int main(){
             case 2:
             float fahrenheit;
             fahrenheit = (9.0/5.0*body_temperature) + 32;
-            cout << "Your Body Temperature in Fahrenheit is " << fahrenheit << endl; 
+            cout << "Your Body Temperature in Fahrenheit is " << fahrenheit << " F" << endl; 
             cout << "Enter Your Choice (1-3):\n1.Fahrenheit to Celsius\n2.Celsius to Fahrenheit\n3.Exit" << " ";
             cin >> choice;
             cout << "Enter the Body Temperature " << " ";
