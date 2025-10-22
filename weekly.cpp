aa877c2c0595833b39c1e741891d80035ea8503f
K.Vijaya Sai <kattamudivijaysai@gmail.com>
2025-10-22 15:08:32 +0530

    Project Done

--- weekly.cpp
+++ weekly.cpp
@@ -16,26 +16,32 @@ int main(){
     //now we will ask the user the select the choice of operation
     cout << "Enter Your Choice (1-3):\n1.Fahrenheit to Celsius\n2.Celsius to Fahrenheit\n3.Exit" << " ";
     cin >> choice;
-    //now performing the choice
-    switch(choice){
-        case 1:
-        float celsius = 5/9*(body_temperature-32);
-        cout << "The Body Temparature in Celsius is " << celsius  << endl;
+    //now performing the choic
+    while(choice != 3){
+        switch(choice){
+            case 1:
+            float celsius;
+            celsius = 5.0/9.0*(body_temperature-32);
+            cout << "The Body Temparature in Celsius is " << celsius << endl;
         //Now we will ask the user again if he wants to perform the operation or not
-        cout << "Enter the Body Temperature " << " ";
-        cin >> body_temperature;
-        cout << "Enter Your Choice (1-3):\n1.Fahrenheit to Celsius\n2.Celsius to Fahrenheit\n3.Exit" << " ";
-        cin >> choice;
-        case 2:
-        float fahrenheit = (9/5*body_temperature) + 32;
-        cout << "Your Body Temperature in Fahrenheit is " << fahrenheit << endl; 
-        cout << "Enter the Body Temperature " << " ";
-        cin >> body_temperature;
-        cout << "Enter Your Choice (1-3):\n1.Fahrenheit to Celsius\n2.Celsius to Fahrenheit\n3.Exit" << " ";
-        cin >> choice;
-        case 3:
-        break;
+            cout << "Enter Your Choice (1-3):\n1.Fahrenheit to Celsius\n2.Celsius to Fahrenheit\n3.Exit" << " ";
+            cin >> choice;
+            cout << "Enter the Body Temperature " << " ";
+            cin >> body_temperature;
+            break;
+            case 2:
+            float fahrenheit;
+            fahrenheit = (9.0/5.0*body_temperature) + 32;
+            cout << "Your Body Temperature in Fahrenheit is " << fahrenheit << endl; 
+            cout << "Enter Your Choice (1-3):\n1.Fahrenheit to Celsius\n2.Celsius to Fahrenheit\n3.Exit" << " ";
+            cin >> choice;
+            cout << "Enter the Body Temperature " << " ";
+            cin >> body_temperature;
+            break;
+            case 3:
+            break;
      }
+    }
     cout << "Thank You For Utilizing the Services" << endl;
     return 0;
 
